64e08f0eea2bebe21395dacf5facffea6a313414
claubuck <camposclaudio@live.com>
2024-10-10 12:05:04 -0300

    add z index

--- resources/js/Components/Notifications.vue
+++ resources/js/Components/Notifications.vue
@@ -2,7 +2,7 @@
     <!-- Global notification live region, render this permanently at the end of the document -->
     <div
       aria-live="assertive"
-      class="mt-10 pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6"
+      class="mt-10 pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6 z-50"
     >
       <div class="flex w-full flex-col items-center space-y-4 sm:items-end">
         <!-- Success notification panel -->
