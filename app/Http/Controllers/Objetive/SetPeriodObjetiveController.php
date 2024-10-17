42a5cc3cc6a4f0bd3cea57f6cd8b791fb2b2c7a0
claubuck <camposclaudio@live.com>
2024-10-17 14:30:58 -0300

    fix selected onjetive period

--- app/Http/Controllers/Objetive/SetPeriodObjetiveController.php
+++ app/Http/Controllers/Objetive/SetPeriodObjetiveController.php
@@ -5,13 +5,18 @@ namespace App\Http\Controllers\Objetive;
 use App\Models\Objetive;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 
 class SetPeriodObjetiveController extends Controller
 {
     public function __invoke(Request $request)
     {
         $objetive = Objetive::find($request->input('objetive_id'));
-        $field = $request->input('comparison_period') === "quantity" ? 'compare_period' : 'compare_period_secondary';
+        $compare_period = Carbon::parse($objetive->compare_period)->format('Y-m');
+        $comparison_period = Carbon::parse($request->input('comparison_period'))->format('Y-m');
+
+        $field = $compare_period === $comparison_period ? 'compare_period' : 'compare_period_secondary';
+
         $value = $objetive->pluck($field)->first();
 
         $objetive->update([
